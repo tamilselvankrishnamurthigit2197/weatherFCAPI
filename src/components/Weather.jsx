77d84a8659c4d459d9b3cb34da347b6367cab6aa
tamilselvankrishnamurthigit2197 <tamilselvanmoorthi97@gmail.com>
2025-03-01 16:26:10 +0530

    forecast url is changed for 4day forecast

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -30,7 +30,7 @@ const Weather = () => {
     // Fetch Forecast Data
     const fetchForecast = async (city) => {
         try {
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
+            const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
             const response = await fetch(url);
             const data = await response.json();
 
